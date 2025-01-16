dfa113b0132eb10b4f56717bad5df34ce3fa83fe
Marcello <marcello.avoni@gmail.com>
2025-01-16 16:21:55 +0100

    improved finger video

--- plot_functions/plot_finger.py
+++ plot_functions/plot_finger.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from numpy import sin, cos
+import numpy as np
 from matplotlib.animation import FuncAnimation
 
 def meters_to_points(meters):
@@ -10,8 +10,8 @@ def meters_to_points(meters):
 
 def rototranslation_given_angle(alpha,x,y,x_o,y_o):
 
-    x_rot = x*cos(alpha) + y*sin(alpha) + x_o
-    y_rot = - x*sin(alpha) + y*cos(alpha) + y_o
+    x_rot = x*np.cos(alpha) + y*np.sin(alpha) + x_o
+    y_rot = - x*np.sin(alpha) + y*np.cos(alpha) + y_o
 
     return x_rot, y_rot
 
@@ -21,34 +21,49 @@ def rototranslation_given_angle(alpha,x,y,x_o,y_o):
 def plot_phalanx(ax,r_1,r_2,L_phalanx,theta,x_o,y_o,alpha):
 
     # we calculate the main points positions in the local reference frame
-    x_1 = 2*r_1*sin(theta/2)
-    y_1 = 2*r_1*cos(theta/2)
+    x_1 = 2*r_1*np.sin(theta/2)
+    y_1 = 2*r_1*np.cos(theta/2)
 
-    x_1_phalanx = x_1 - r_1*cos(theta)
-    y_1_phalanx = y_1 + r_1*sin(theta)
+    x_1_phalanx_up = x_1 - r_1*np.cos(theta)
+    y_1_phalanx_up = y_1 + r_1*np.sin(theta)
 
-    x_2_phalanx = x_1_phalanx + (L_phalanx-r_1-r_2)*sin(theta)
-    y_2_phalanx = y_1_phalanx + (L_phalanx-r_1-r_2)*cos(theta)
+    x_2_phalanx_up = x_1_phalanx_up + (L_phalanx-r_1-r_2)*np.sin(theta)
+    y_2_phalanx_up = y_1_phalanx_up + (L_phalanx-r_1-r_2)*np.cos(theta)
 
-    x_2 = x_2_phalanx + r_2*cos(theta)
-    y_2 = y_2_phalanx - r_2*sin(theta)
+    x_2 = x_2_phalanx_up + r_2*np.cos(theta)
+    y_2 = y_2_phalanx_up - r_2*np.sin(theta)
+
+    x_1_phalanx_down = x_1 + r_1*np.cos(theta)
+    y_1_phalanx_down = y_1 - r_1*np.sin(theta)
+
+    x_2_phalanx_down = x_2 + r_2*np.cos(theta)
+    y_2_phalanx_down = y_2 - r_2*np.sin(theta)
+
 
     # we rotate  and translate so to stay in the absolute reference frame
     x_1, y_1 = rototranslation_given_angle(alpha,x_1,y_1,x_o,y_o)
-    x_1_phalanx, y_1_phalanx = rototranslation_given_angle(alpha,x_1_phalanx,y_1_phalanx,x_o,y_o)
-    x_2_phalanx, y_2_phalanx = rototranslation_given_angle(alpha,x_2_phalanx,y_2_phalanx,x_o,y_o)
+    x_1_phalanx_up, y_1_phalanx_up = rototranslation_given_angle(alpha,x_1_phalanx_up,y_1_phalanx_up,x_o,y_o)
+    x_2_phalanx_up, y_2_phalanx_up = rototranslation_given_angle(alpha,x_2_phalanx_up,y_2_phalanx_up,x_o,y_o)
     x_2, y_2 = rototranslation_given_angle(alpha,x_2,y_2,x_o,y_o)
+    x_1_phalanx_down, y_1_phalanx_down = rototranslation_given_angle(alpha,x_1_phalanx_down,y_1_phalanx_down,x_o,y_o)
+    x_2_phalanx_down, y_2_phalanx_down = rototranslation_given_angle(alpha,x_2_phalanx_down,y_2_phalanx_down,x_o,y_o)
 
+    # we define 2 circumferences that represent the joints
+    theta = np.linspace(0,2*np.pi,100)
+    x_circ_1 = x_1 + r_1*np.sin(theta)
+    y_circ_1 = y_1 + r_1*np.cos(theta)
 
-
+    x_circ_2 = x_2 + r_2*np.sin(theta)
+    y_circ_2 = y_2 + r_2*np.cos(theta)
 
     #we overlay lines that represent current phalanx
-    ax.plot([x_1,x_2],[y_1,y_2],color='black',linewidth=2,linestyle='-')
-    ax.plot([x_1_phalanx,x_2_phalanx],[y_1_phalanx,y_2_phalanx],color='black',linewidth=2,linestyle='-')
+    ax.plot([x_1,x_2],[y_1,y_2],color='black',linewidth=1,linestyle='--')
+    ax.plot([x_1_phalanx_up,x_2_phalanx_up],[y_1_phalanx_up,y_2_phalanx_up],color='black',linewidth=2,linestyle='-')
+    ax.plot([x_1_phalanx_down,x_2_phalanx_down],[y_1_phalanx_down,y_2_phalanx_down],color='black',linewidth=2,linestyle='-')
 
     #we overlay joints
-    ax.plot([x_1],[y_1],marker='o',color='red',markersize=r_1)
-    ax.plot([x_2],[y_2],marker='o',color='red',markersize=r_2)
+    ax.plot(x_circ_1,y_circ_1,color='black',linewidth=2,linestyle='-')
+    ax.plot(x_circ_2,y_circ_2,color='black',linewidth=2,linestyle='-')
 
     return x_2, y_2
 
@@ -87,7 +102,13 @@ def plot_finger(ax,joint_angles,r_joints,r_tip,L_phalanxes):
     #now we plot the finger
     x_0 = 0
     y_0 = 0
-    ax.plot(x_0,y_0,marker='o',color='red')
+
+    angles = np.linspace(0,2*np.pi,100)
+    x_circ = x_0 + r_joints[0]*np.sin(angles)
+    y_circ = y_0 + r_joints[0]*np.cos(angles)
+
+    ax.plot(x_circ,y_circ,color='black',linewidth=2,linestyle='-')
+
 
     alpha = 0
 
@@ -111,6 +132,7 @@ def make_animation(joint_angles,r_joints,r_tip,L_phalanxes):
 
     # we iterate over the joints to print the finger
     fig, ax = plt.subplots()
+
 
     ani = FuncAnimation(fig,plot_update,frames=len(joint_angles),fargs=(ax,joint_angles,r_joints,r_tip,L_phalanxes),repeat=False)
     plt.show()
